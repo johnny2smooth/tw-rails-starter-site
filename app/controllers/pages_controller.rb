2a9f0cf83ea3524fcdef2efdf37d4ae31cfc294c
Johnny R. Bee <jbp13@uw.edu>
2025-08-06 09:35:00 -0700

    presenter pattern introduced to decrease controller bloat.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
   def index
+    @page = HomePagePresenter.new
     @nav_links = [
       { title: 'Product',     path: '#' },
       { title: 'Features',    path: '#' },
